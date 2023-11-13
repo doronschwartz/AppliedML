ace2510573dbdeb1c9e613100bd070bda8b65939
doronschwartz <77128658+doronschwartz@users.noreply.github.com>
2023-11-12 23:28:39 -0500

    Google Cloud Deployed

---
+++ IEEE-GC/deploy.py
@@ -0,0 +1,60 @@
+from google.cloud import aiplatform
+import json
+import pandas as pd
+
+
+project_id = "big-celerity-404704"
+location = "us-east4"
+endpoint_id = "6739469716592525312"
+
+
+original_row = pd.read_csv('/Users/doronschwartz/YCCS/AppliedML/IEEE-GC/data/train_transaction.csv', nrows=1)
+
+original_data = original_row.iloc[0].astype(str).to_dict()
+
+# Initialize AI Platform'
+aiplatform.init(project=project_id, location=location)
+
+# Get the endpoint
+endpoint = aiplatform.Endpoint(endpoint_name=f"projects/{project_id}/locations/{location}/endpoints/{endpoint_id}")
+
+# Prompt user for input
+transaction_id = input("Enter TransactionId: ")
+product_cd = input("Enter ProductCD: ")
+transaction_amt = input("Enter TransactionAmt: ")
+card4 = input("Enter Card Type (card4): ")
+card6 = input("Enter Card Category (card6): ")
+p_emaildomain = input("Enter P_emaildomain: ")
+
+original_data['TransactionId'] = transaction_id
+original_data['ProductCD'] = product_cd
+original_data['TransactionAmt'] = transaction_amt
+original_data['card4'] = card4
+original_data['card6'] = card6
+original_data['P_emaildomain'] = p_emaildomain
+
+# Convert input_data to JSON format
+json_data = json.dumps({'instances': [original_data]})
+
+# Make a prediction
+response = endpoint.predict(instances=[original_data])
+
+print("Prediction Response:")
+# Extract prediction scores
+prediction_scores = response.predictions[0]['scores']
+class_labels = response.predictions[0]['classes']
+
+# Print prediction scores
+print("Prediction Scores:")
+for label, score in zip(class_labels, prediction_scores):
+    print(f"{label}: {score}")
+
+# Interpret prediction
+predicted_class = class_labels[prediction_scores.index(max(prediction_scores))]
+
+
+# Print interpretation
+if predicted_class == '1':
+    print(" Likely Fraudulent Transaction")
+else:
+    print("Likely Non-Fraudulent Transaction")
